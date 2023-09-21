cab9164e1abe4e6b8cdc45d94112597b42b00718
Kat Tannehill <katetannehill@gmail.com>
2023-09-21 07:40:20 -0600

    Create global variables

--- src/index.js
+++ src/index.js
@@ -3,7 +3,15 @@
 
 
 // Global variables
-
+const URL = "http://localhost:3000/ramens"
+const ramenMenu = document.querySelector("#ramen-menu")
+const ramenDetails = document.querySelector("#ramen-detail")
+const ramenImg = document.querySelector("img.detail-image")
+const ramenName = document.querySelector("h2.name")
+const restaurant = document.querySelector("h3.restaurant")
+const rating = document.querySelector("#rating-display")
+const comment = document.querySelector("#comment-display")
+const newRamenForm = document.querySelector("#new-ramen")
 
 
 // Helper functions
